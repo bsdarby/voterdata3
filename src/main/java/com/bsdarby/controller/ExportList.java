04bfb615c7c36862656211ae9346485253fb655d
Brian Darby <bsdarby@gmail.com>
2014-09-30 13:57:34 -0700

    Begin Export

--- src/main/java/com/bsdarby/controller/ExportList.java
+++ src/main/java/com/bsdarby/controller/ExportList.java
@@ -5,6 +5,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.sql.ResultSet;
 
 /**
@@ -12,52 +13,77 @@ import java.sql.ResultSet;
  * Created by bsdarby on 9/30/14.
  */
 public class ExportList extends JFrame{
-
-	private JPanel contentPane;
-	private JFileChooser fileChooser = new JFileChooser();
-	private JButton btnSave, btnBrowse;
-	private JLabel lblFolderName = new JLabel("No folder selected.");
-	private Desktop desktop = Desktop.getDesktop();
-	private JTextArea textArea = new JTextArea();
+	File exportFile;
+	JButton btnSave, btnBrowse;
+	JFileChooser fileChooser = new JFileChooser();
+	JLabel lblFilePath;
 
 	public ExportList(ResultSet resultSet){
+		JPanel contentPane, pnlExpCentral, pnlExpSouth, pnlExpNorth;
+		lblFilePath = new JLabel("No file selected");
+		Desktop desktop = Desktop.getDesktop();
+		JTextArea textArea = new JTextArea();
+
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		setSize(new Dimension(400, 400));
 		setLocationRelativeTo(null);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5,5,5,5));
+		contentPane = new JPanel(new BorderLayout());
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout());
 
-		btnBrowse = new JButton("Choose Save Location");
+		pnlExpCentral	= new JPanel(new FlowLayout());
+		pnlExpNorth 	= new JPanel(new FlowLayout());
+		pnlExpSouth 	= new JPanel(new FlowLayout());
+
+
+		btnBrowse = new JButton("Choose File");
 		btnBrowse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				openSaveDialog(fileChooser, lblFolderName, true);
+				openSaveDialog(fileChooser);
 		}});
 		contentPane.add(btnBrowse);
 
-		btnSave = new JButton("Save");
+		btnSave = new JButton("Export");
 		btnSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				saveExportFile();
 			}
 		});
+		btnSave.setEnabled(false);
 
-		JLabel lblFolder = new JLabel("Folder:");
-		JPanel pnlExportNorth = new JPanel(new GridLayout(0,2));
-		pnlExportNorth.add(lblFolder);
-		pnlExportNorth.add(lblFolderName);
-		contentPane.add(pnlExportNorth);
-		contentPane.add(btnSave, BorderLayout.SOUTH);
+		JLabel lblFile = new JLabel("File:");
+		pnlExpNorth.add(lblFile);
+		pnlExpNorth.add(lblFilePath);
+		contentPane.add(pnlExpNorth, BorderLayout.NORTH);
+
+		pnlExpSouth.add(btnBrowse);
+		pnlExpSouth.add(btnSave);
+		contentPane.add(pnlExpSouth, BorderLayout.SOUTH);
+
+
 		validate();
 		setVisible(true);
 
 
 	}
 
-	private void openSaveDialog(JFileChooser fileChooser, JLabel lblFolderName, boolean directoryOnly){
-		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-
+	private void openSaveDialog(JFileChooser fileChooser){
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		int returnVal = fileChooser.showSaveDialog(this);
+		System.out.println("fileChooser: returnVal = "+ returnVal +", Selected: "+ fileChooser.getSelectedFile());
+		if (JFileChooser.APPROVE_OPTION == returnVal) {
+			exportFile = fileChooser.getSelectedFile();
+			int pathLength = exportFile.getPath().length();
+			String strLabelMsg;
+			if(50 < pathLength) {
+				strLabelMsg = "...";
+				strLabelMsg += exportFile.getPath().substring(pathLength - 50);
+			} else {
+				strLabelMsg  = exportFile.getPath();
+			}
+			lblFilePath.setText(strLabelMsg);
+			btnSave.setEnabled(true);
+		}
 	}
 
 	private void saveExportFile(){
